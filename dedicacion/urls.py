c4365e1365d0ad377000cdbf6a69b5ea6908b377
Dario-M-Sinani <dariomichel1992@gmail.com>
2025-10-18 21:42:42 -0400

    trando deploy4

--- dedicacion/urls.py
+++ dedicacion/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('carta/', views.carta_secreta, name='carta'),
     path('recuerdos/', views.nuestros_recuerdos, name='recuerdos'),
     path('amor/', views.mi_amor, name='amor'),
