a2443fb2e2dc34daeb05b127ece7cd198d77fa5a
Dario-M-Sinani <dariomichel1992@gmail.com>
2025-10-18 21:33:34 -0400

    trando deploy2

--- build.sh
+++ build.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
 
-# 1. Instalación de Node y Construcción de Frontend
-echo "-> Instalando dependencias de Node..."
-# Instala Tailwind (la única dependencia de Node)
-npm install
-
-# Ejecuta el script 'build' recién añadido del package.json para generar el CSS final.
-echo "-> Ejecutando npm run build para compilar CSS..."
-npm run build 
-
-# 2. Instalación de Python
-echo "-> Instalando dependencias de Python..."
-# Aquí se asegura la instalación de Django, Gunicorn, Whitenoise, etc.
+# Solo Python
 pip install -r requirements.txt
-
-# 3. Recolección de Archivos Estáticos de Django
-# Esto recoge el CSS generado en el paso 1 junto con los estáticos de Django.
-echo "-> Recolectando archivos estáticos de Django..."
 python manage.py collectstatic --noinput --clear
